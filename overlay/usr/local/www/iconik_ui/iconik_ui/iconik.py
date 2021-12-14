8210acac8177ef88008f92082fea1ce4fbd0c444
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-12-14 15:14:21 +0500

    Update imports to comply with python39 change

--- overlay/usr/local/www/iconik_ui/iconik_ui/iconik.py
+++ overlay/usr/local/www/iconik_ui/iconik_ui/iconik.py
@@ -5,7 +5,7 @@ import urllib.parse
 from werkzeug.wrappers import Request, Response
 from werkzeug.routing import Map, Rule
 from werkzeug.exceptions import HTTPException, NotFound
-from werkzeug.wsgi import SharedDataMiddleware
+from werkzeug.middleware.shared_data import SharedDataMiddleware
 from werkzeug.utils import redirect
 
 from jinja2 import Environment, FileSystemLoader
