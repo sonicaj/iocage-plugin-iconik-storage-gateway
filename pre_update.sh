d45af6b184fd66c1e986646082fcf539d11c3dda
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-12-14 16:22:55 +0500

    Add a pre update hook to save iconik credentials

---
+++ pre_update.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+config_file="/usr/local/etc/cantemo/iconik_storage_gateway/config.ini"
+backup_config="/tmp/credentials_backup"
+
+rm -f "$backup_config"
+
+for key in "app-id" "storage-id"; do
+	v=$(sed -n "s/.*$key *= *\([^ ]*.*\)/\1/p" < "$config_file")
+	echo "$key = $v" >> "$backup_config"
+done
